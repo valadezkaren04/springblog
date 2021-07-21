a813662e89575b19bfa4c62a5ebdcef12a707b80
Karen Valadez <valadez.karen04@gmail.com>
2021-07-21 11:11:02 -0500

    commit

---
+++ src/main/java/com/codeup/springblog/controllers/RollDiceController.java
@@ -0,0 +1,14 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RollDiceController {
+
+    @GetMapping("/roll-dice")
+    public String rollDice() {
+        return "Guess a number...";
+    }
+
+}
