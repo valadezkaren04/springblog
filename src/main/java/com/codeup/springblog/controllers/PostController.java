236b33254f9b3c740fae8f654338ed1a522993d0
Karen Valadez <valadez.karen04@gmail.com>
2021-07-22 13:14:46 -0500

    exercise functionality complete ; need to add footer

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,29 +1,45 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
+    private List<Post> posts = new ArrayList<>();
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String postIndex() {
-        return "View all posts.";
+//    @ResponseBody //taken out due to views exercise
+    public String postIndex(Model model) { //references back to the posts
+        posts.add(new Post("This is post1", "This is post1s body"));
+        posts.add(new Post("This is post2", "This is post2s body"));
+        model.addAttribute("posts", posts);
+        return "posts/index";
+//        return "View all posts."; //taken out due to views exercise
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String individualPost(@PathVariable long id) {
-        return "View individual post.";
+//    @ResponseBody
+    public String individualPost(@PathVariable long id, Model model) {
+        Post post =  new Post("BT21 are the cutest character", "I want all of the BT21 plushies please!");
+        model.addAttribute("post", post);
+        return "posts/show";
+//        return "View individual post.";
     }
 
+    // When you visit the URL you will see the form to create a post.
     @GetMapping("/posts/create")
     @ResponseBody
     public String viewCreatePost() {
         return "View your created post.";
     }
 
+    // When you submit the form on the /posts/create page,
+    // the information will be posted to the same URL
 //    @GetMapping("/posts/create")
     @RequestMapping(path = "/post/create", method = RequestMethod.POST)
     @ResponseBody
