20321cfaf5c9d21658e2eb566ce6a31329170b5f
Karen Valadez <valadez.karen04@gmail.com>
2021-07-23 15:50:04 -0500

    id giving an error, leading to a white label error in server

---
+++ src/main/resources/sql/seeder.sql
@@ -0,0 +1,3 @@
+USE springblog_db;
+
+INSERT INTO posts (body, title) VALUES ('The only time you should ever look back, is to see how far you''ve come.', 'BTS'), ('My dream isn’t to become the ‘best’; it’s to be someone who I’m not ashamed to be.', 'Key, Shinee'), ('There''s no shortcuts to perfection. All it takes is hard work and more hard work.', 'Jonghyun, Shinee'), ('All aboard this trip to hell bus.', 'Jeonghan, Seventeen');
