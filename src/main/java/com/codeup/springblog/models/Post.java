ca2b938e53be511f249074ba76a5a3b6b97fc301
Karen Valadez <valadez.karen04@gmail.com>
2021-07-23 15:53:20 -0500

    id error fixed ; exercise functionality complete. Add styling

--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -21,6 +21,10 @@ public class Post {
     public Post() {
     }
 
+    public Post(long id) {
+        this.id = id;
+    }
+
     public Post(String title, String body) {
         this.title = title;
         this.body = body;
@@ -40,5 +44,13 @@ public class Post {
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 }
