4754a591f3a46e0b274a6c2f14b87af50864ef17
Karen Valadez <valadez.karen04@gmail.com>
2021-08-03 09:23:03 -0500

    commit

--- src/test/java/com/codeup/springblog/PostIntegrationTests.java
+++ src/test/java/com/codeup/springblog/PostIntegrationTests.java
@@ -19,6 +19,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import javax.servlet.http.HttpSession;
+import java.util.List;
+
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -116,38 +118,39 @@ public class PostIntegrationTests {
     }
 
     @Test
-    public void testAdsIndex() throws Exception {
-        Ad existingAd = adsDao.findAll().get(0);
+    public void testPostsIndex() throws Exception {
+        List<Post>
+        Post existingPost = postRepo.findAll().get(0);
 
         // Makes a Get request to /ads and verifies that we get some of the static text of the ads/index.html template and at least the title from the first Ad is present in the template.
-        this.mvc.perform(get("/ads"))
+        this.mvc.perform(get("/posts"))
                 .andExpect(status().isOk())
                 // Test the static content of the page
-                .andExpect(content().string(containsString("Latest ads")))
+                .andExpect(content().string(containsString("Viewing all posts")))
                 // Test the dynamic content of the page
-                .andExpect(content().string(containsString(existingAd.getTitle())));
+                .andExpect(content().string(containsString(existingPost.getTitle())));
     }
 
     //    Update. This test will focus on an existing Ad and change it's title and description. You will notice that we are using the CSRF token again and the session since it's required to be logged in to perform this action.
     @Test
-    public void testEditAd() throws Exception {
+    public void testEditPost() throws Exception {
         // Gets the first Ad for tests purposes
-        Ad existingAd = adsDao.findAll().get(0);
+        Post existingPost = postRepo.findByUsername("testUser").getPosts.get(0);
 
         // Makes a Post request to /ads/{id}/edit and expect a redirection to the Ad show page
         this.mvc.perform(
-                post("/ads/" + existingAd.getId() + "/edit").with(csrf())
+                post("/posts/" + existingPost.getId() + "/edit").with(csrf())
                         .session((MockHttpSession) httpSession)
                         .param("title", "edited title")
-                        .param("description", "edited description"))
+                        .param("body", "edited body"))
                 .andExpect(status().is3xxRedirection());
 
         // Makes a GET request to /ads/{id} and expect a redirection to the Ad show page
-        this.mvc.perform(get("/ads/" + existingAd.getId()))
+        this.mvc.perform(get("/posts/" + existingPost.getId()))
                 .andExpect(status().isOk())
                 // Test the dynamic content of the page
                 .andExpect(content().string(containsString("edited title")))
-                .andExpect(content().string(containsString("edited description")));
+                .andExpect(content().string(containsString("edited body")));
     }
 
     // Delete. For this test we will create an Ad and destroy it to verify if it's gone and redirects back to the ("/ads") URL.
@@ -162,7 +165,7 @@ public class PostIntegrationTests {
                 .andExpect(status().is3xxRedirection());
 
         // Get the recent Ad that matches the title
-        Ad existingAd = adsDao.findByTitle("ad to be deleted");
+        Post existingPost = postRepo.findByTitle("ad to be deleted");
 
         // Makes a Post request to /ads/{id}/delete and expect a redirection to the Ads index
         this.mvc.perform(
